3d0597b9d1abe20656701978ed657c024224cd26
clementine@huggingface.co <clementine@huggingface.co>
2024-02-22 14:33:43 +0000

    added doc following nathan comments

--- community_tasks/_template.py
+++ community_tasks/_template.py
@@ -11,7 +11,25 @@ from lighteval.tasks.requests import Doc
 from lighteval.tasks.tasks_prompt_formatting import LETTER_INDICES
 
 
-## EVAL WITH SUBSET ##
+## EVAL WITH NO SUBSET ##
+# This is how you create a simple tasks (like hellaswag) which has one single subset
+# attached to it, and one evaluation possible.
+task = LightevalTaskConfig(
+    name="myothertask",
+    prompt_function="prompt_fn",  # must be defined in the file or imported from src/lighteval/tasks/tasks_prompt_formatting.py
+    suite=["community"],
+    hf_repo="",
+    hf_subset="default",
+    hf_avail_splits=[],
+    evaluation_splits=[],
+    few_shots_split="",
+    few_shots_select="",
+    metric=[""],
+)
+
+## EVALS WITH SUBSET
+# This is how you create a subset task (like MMLU), which has several subset
+# each being its own evaluation task.
 
 # fmt: off
 SAMPLE_SUBSETS = [] # list of all the subsets to use for this eval
@@ -42,12 +60,13 @@ class CustomSubsetTask(LightevalTaskConfig):
         )
 
 
-SUBSET_TASKS = [CustomSubsetTask(name=f"mytask:{subset}", hf_subset=subset) for subset in SAMPLE_SUBSETS]
-
-
-# Follow examples in src/lighteval/tasks/tasks_prompt_formatting.py
-# Note that the input line is a line of your dataset
+## DEFINE YOUR PROMPT FUNCTIONS
+# Define as many as you need for your different tasks
 def prompt_fn(line, task_name: str = None):
+    """Defines how to go from a dataset line to a doc object.
+    Follow examples in src/lighteval/tasks/tasks_prompt_formatting.py, or get more info
+    about what this function should do in the README.
+    """
     return Doc(
         task_name=task_name,
         query="",
@@ -57,23 +76,12 @@ def prompt_fn(line, task_name: str = None):
     )
 
 
-## EVAL WITH NO SUBSET ##
-task = LightevalTaskConfig(
-    name="mytask",
-    prompt_function="prompt_fn",
-    suite=["community"],
-    hf_repo="",
-    hf_subset="default",
-    hf_avail_splits=[],
-    evaluation_splits=[],
-    few_shots_split="",
-    few_shots_select="",
-    metric=[""],
-)
-
-
+## STORE YOUR EVALS
+SUBSET_TASKS = [CustomSubsetTask(name=f"mytask:{subset}", hf_subset=subset) for subset in SAMPLE_SUBSETS]
 _TASKS = SUBSET_TASKS + [task]
 
+## MODULE LOGIC
+# You should not need to touch this
 # Convert to dict for lighteval
 TASKS_TABLE = [task.as_dict() for task in _TASKS]
 
