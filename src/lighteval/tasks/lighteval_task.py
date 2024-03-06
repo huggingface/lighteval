3b0aa23d82323e20583608175565de4d77b443e7
Clémentine Fourrier <22726840+clefourrier@users.noreply.github.com>
2024-03-06 12:19:08 +0100

    Fix parallel data processing bug (#92)
    * fix
    
    * fix for no //
    
    * Update lighteval_task.py

--- src/lighteval/tasks/lighteval_task.py
+++ src/lighteval/tasks/lighteval_task.py
@@ -303,7 +303,7 @@ class LightevalTask:
             list[Doc]: List of documents.
         """
         if self.dataset is None:
-            self.dataset = download_dataset_worker((self.dataset_path, self.dataset_config_name))
+            self.dataset = download_dataset_worker((self.dataset_path, self.dataset_config_name, self.trust_dataset))
 
         docs = []
         for split in splits:
@@ -546,10 +546,16 @@ class LightevalTask:
         """
 
         if dataset_loading_processes <= 1:
-            datasets = [download_dataset_worker(task) for task in tasks]  # Also help us with gdb
+            datasets = [
+                download_dataset_worker((task.dataset_path, task.dataset_config_name, task.trust_dataset))
+                for task in tasks
+            ]
         else:
             with Pool(processes=dataset_loading_processes) as pool:
-                datasets = pool.map(download_dataset_worker, tasks)
+                datasets = pool.map(
+                    download_dataset_worker,
+                    [(task.dataset_path, task.dataset_config_name, task.trust_dataset) for task in tasks],
+                )
 
         for task, dataset in zip(tasks, datasets):
             task.dataset = dataset
@@ -560,14 +566,14 @@ def download_dataset_worker(args):
     Worker function to download a dataset from the HuggingFace Hub.
     Used for parallel dataset loading.
     """
-    task: LightevalTask = args
+    dataset_path, dataset_config_name, trust_dataset = args
     dataset = load_dataset(
-        path=task.dataset_path,
-        name=task.dataset_config_name,
+        path=dataset_path,
+        name=dataset_config_name,
         data_dir=None,
         cache_dir=None,
         download_mode=DownloadMode.FORCE_REDOWNLOAD,  # None
-        trust_remote_code=task.trust_dataset,
+        trust_remote_code=trust_dataset,
     )
     return dataset
 
