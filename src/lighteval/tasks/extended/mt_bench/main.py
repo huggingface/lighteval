bb4b1333fddbb453b15efd3a656abc78092e9be9
Nathan Habib <nathan.habib@huggingface.co>
2024-03-29 12:44:58 +0000

    make style

--- src/lighteval/tasks/extended/mt_bench/main.py
+++ src/lighteval/tasks/extended/mt_bench/main.py
@@ -45,7 +45,11 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 if OPENAI_API_KEY is None:
     # Using print here because hlog_warn is not yet available in this context
-    print(Fore.YELLOW + "No OpenAI API key found. If you are using the OpenAI judge, please set the OPENAI_API_KEY environment variable." + Style.RESET_ALL)
+    print(
+        Fore.YELLOW
+        + "No OpenAI API key found. If you are using the OpenAI judge, please set the OPENAI_API_KEY environment variable."
+        + Style.RESET_ALL
+    )
 
 # EVAL WITH NO SUBSET ##
 # This is how you create a simple tasks (like hellaswag) which has one single subset
@@ -99,7 +103,7 @@ def mt_bench_metric(predictions: list[str], formatted_doc: Doc, **kwargs) -> dic
         seed=42,
         temperature=0.0,
         templates_path="src/lighteval/tasks/extended/mt_bench/judge_prompts.jsonl",
-        openai_api_key=OPENAI_API_KEY
+        openai_api_key=OPENAI_API_KEY,
     )
 
     questions = formatted_doc.specific["multi_turn_queries"]
