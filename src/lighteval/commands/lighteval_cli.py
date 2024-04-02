81deaa0762930a9488955fafabede4d68a4d3264
taratra <dimbimandresy@gmail.com>
2024-04-03 02:09:33 +0300

    changing list_tasks using os.walk

--- src/lighteval/commands/lighteval_cli.py
+++ src/lighteval/commands/lighteval_cli.py
@@ -69,9 +69,10 @@ def list_tasks_command():
         # Handling extended tasks
         tasks_extended = []
         extended_tasks_dir = pkg_resources.resource_filename("lighteval", "tasks/extended")
-        for module_name, dirs, files in os.walk(extended_tasks_dir):
+        for root, dirs, files in os.walk(extended_tasks_dir):
             for file in files:
                 if file == "main.py":
+                    module_name = os.path.basename(root)
                     tasks_table = load_tasks_table_extended(module_name)
                     tasks_extended += tasks_table
         tasks += tasks_extended
