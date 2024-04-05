ba8a886d3ba67d4bb2064afffdd0de3bd42e22b3
taratra <dimbimandresy@gmail.com>
2024-04-05 23:43:40 +0300

    License to The HuggingFace Team

--- src/lighteval/commands/__init__.py
+++ src/lighteval/commands/__init__.py
@@ -1,6 +1,6 @@
 # MIT License
 
-# Copyright (c) 2024 Taratra D. RAHARISON
+# Copyright (c) 2024 The HuggingFace Team
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
