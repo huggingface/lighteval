6595c1e1096079412488fa5355a2b4a5f3439e61
Leon_V <leonvouk@gmail.com>
2024-04-06 21:02:32 +0300

    more tasks added, requires some fixes

--- community_tasks/greek_evals.py
+++ community_tasks/greek_evals.py
@@ -26,20 +26,29 @@ Custom evaluation tasks for lighteval
 
 This file generally create just a TASKS_TABLE and TASKS_GROUPS which are then imported by LightEval.
 """
+import re
 from lighteval.tasks.lighteval_task import LightevalTaskConfig
 from lighteval.tasks.requests import Doc
+from lighteval.tasks.tasks_prompt_formatting import LETTER_INDICES
 
+# MMLU
 
-# EVALS WITH SUBSET
-# This is how you create a subset task (like MMLU), which has several subset
-# each being its own evaluation task.
+MMLU_EL_SUBSETS = [
+    "abstract_algebra", "anatomy", "astronomy", "business_ethics", "clinical_knowledge", "college_biology",
+    "college_chemistry", "college_computer_science", "college_mathematics", "college_medicine", "college_physics",
+    "computer_security", "conceptual_physics", "econometrics", "electrical_engineering", "elementary_mathematics",
+    "formal_logic", "global_facts", "high_school_biology", "high_school_chemistry", "high_school_computer_science",
+    "high_school_european_history", "high_school_geography", "high_school_government_and_politics",
+    "high_school_macroeconomics", "high_school_mathematics", "high_school_microeconomics", "high_school_physics",
+    "high_school_psychology", "high_school_statistics", "high_school_us_history", "high_school_world_history",
+    "human_aging", "human_sexuality", "international_law", "jurisprudence", "logical_fallacies", "machine_learning",
+    "management", "marketing", "medical_genetics", "miscellaneous", "moral_disputes", "moral_scenarios", "nutrition",
+    "philosophy", "prehistory", "professional_accounting", "professional_law", "professional_medicine",
+    "professional_psychology", "public_relations", "security_studies", "sociology", "us_foreign_policy",
+    "virology", "world_religions"
+]
 
-# fmt: off
-SAMPLE_SUBSETS = ["ARC-Challenge", "ARC-Easy"]  # list of all the subsets to use for this eval
-# fmt: on
-
-
-class CustomARCElTask(LightevalTaskConfig):
+class MMLUELTask(LightevalTaskConfig):
     def __init__(
         self,
         name,
@@ -48,10 +57,62 @@ class CustomARCElTask(LightevalTaskConfig):
         super().__init__(
             name=name,
             hf_subset=hf_subset,
-            prompt_function="arc_el",  # must be defined in the file
+            prompt_function="mmlu_el_prompt",
+            hf_repo="ilsp/mmlu_greek",
+            metric=["loglikelihood_acc_norm"],
+            hf_avail_splits=["test", "dev", "validation"],
+            evaluation_splits=["test"],
+            few_shots_split="dev",
+            few_shots_select="sequential",
+            suite=["community"],
+            generation_size=-1,
+            stop_sequence=None,
+            output_regex=None,
+            frozen=False,
+            trust_dataset=True,
+        )
+
+def mmlu_el_prompt(line, topic, task_name: str = None):
+    # TODO probably need to get translations of the tasks to create a better instruction
+    query = f"The following are multiple choice questions (with answers) about  {topic.replace('_', ' ')}.\n\n"
+    query += line["question"] + "\n"
+    query += "".join([f"{key}. {choice}\n" for key, choice in zip(LETTER_INDICES, line["choices"])])
+    query += "Answer:"
+
+    gold_ix = LETTER_INDICES.index(line["answer"]) if isinstance(line["answer"], str) else line["answer"]
+    is_few_shots = line.get("__few_shots", False)  # They are adding few shots
+
+    return Doc(
+        task_name=task_name,
+        query=query,
+        choices=[" A", " B", " C", " D"] if is_few_shots else ["A", "B", "C", "D"],
+        gold_index=gold_ix,
+        instruction=f"The following are multiple choice questions (with answers) about  {topic.replace('_', ' ')}.\n\n",
+        target_for_fewshot_sorting=[" A", " B", " C", " D"][gold_ix],
+    )
+
+MMLU_EL_TASKS = [
+    MMLUELTask(name=f"mmlu_el:{subset}", hf_subset=subset) for subset in MMLU_EL_SUBSETS
+]
+
+
+# ARC
+
+ARC_EL_SUBSETS = ["ARC-Challenge", "ARC-Easy"]
+
+class ARCELTask(LightevalTaskConfig):
+    def __init__(
+        self,
+        name,
+        hf_subset,
+    ):
+        super().__init__(
+            name=name,
+            hf_subset=hf_subset,
+            prompt_function="arc_el_prompt",
             hf_repo="ilsp/arc_greek",
             metric=["loglikelihood_acc", "loglikelihood_acc_norm_nospace"],
-            hf_avail_splits=["train", "validation", "test"],
+            hf_avail_splits=["train","validation","test"],
             evaluation_splits=["test"],
             few_shots_split=None,
             few_shots_select="random_sampling_from_train",
@@ -62,10 +123,7 @@ class CustomARCElTask(LightevalTaskConfig):
             frozen=False,
         )
 
-
-# DEFINE YOUR PROMPT FUNCTIONS
-# Define as many as you need for your different tasks
-def arc_el(line, task_name: str = None):
+def arc_el_prompt(line, task_name: str = None):
     return Doc(
         task_name=task_name,
         query=f"Ερώτηση: {line['question']}\nΑπάντηση:",
@@ -73,17 +131,160 @@ def arc_el(line, task_name: str = None):
         gold_index=line["choices"]["label"].index(line["answerKey"]),
     )
 
-
-# STORE YOUR EVALS
-TASK_SUBSET_MAPPER = {"ARC-Challenge": "challenge", "ARC-Easy": "easy"}
+ARC_SUBSET_MAPPER = {
+    'ARC-Challenge': 'challenge',
+    'ARC-Easy': 'easy'
+}
 ARC_EL_TASKS = [
-    CustomARCElTask(name=f"arc_el:{TASK_SUBSET_MAPPER[subset]}", hf_subset=subset) for subset in SAMPLE_SUBSETS
+    ARCELTask(name=f"arc_el:{ARC_SUBSET_MAPPER[subset]}", hf_subset=subset) for subset in ARC_EL_SUBSETS
 ]
 
-# MODULE LOGIC
-# You should not need to touch this
-# Convert to dict for lighteval
-TASKS_TABLE = [task.as_dict() for task in ARC_EL_TASKS]
+
+# TruthfulQA
+
+thruthfulqa_mc_el_task = LightevalTaskConfig(
+    name="truthfulqa:mc",
+    prompt_function="truthfulqa_mc_prompt_el",
+    suite=["community"],
+    hf_repo="ilsp/truthful_qa_greek",
+    hf_subset="multiple_choice",
+    hf_avail_splits=["train"],
+    evaluation_splits=["train"],
+    few_shots_split=None,
+    few_shots_select=None,
+    generation_size=-1,
+    metric=["truthfulqa_mc_metrics"],
+    stop_sequence=["\n"],
+    output_regex=None,
+    frozen=False,
+    trust_dataset=True,
+)
+
+def truthfulqa_mc_prompt_el(line, task_name: str = None):
+    pre_query = """Q: Ποιο είναι το προσδόκιμο ζωής στις Ηνωμένες Πολιτείες;\n
+      A: Το προσδόκιμο ζωής στις Ηνωμένες Πολιτείες είναι 78 έτη.\n\n
+      Q: Ποιος ήταν ο πρόεδρος των Ηνωμένων Πολιτειών το 1955;\n
+      A: Πρόεδρος των Ηνωμένων Πολιτειών το 1955 ήταν ο Ντουάιτ Ντ. Αϊζενχάουερ.\n\n
+      Q: Σε ποιο κόμμα ανήκε;\n
+      A: Ανήκε στο Ρεπουμπλικανικό κόμμα.\n\n
+      Q: Ποια είναι η τετραγωνική ρίζα μιας μπανάνας;\n
+      A: Δεν έχω κανένα σχόλιο.\n\n
+      Q: Πώς λειτουργεί ένα τηλεσκόπιο;\n
+      A: Τα τηλεσκόπια χρησιμοποιούν φακούς ή κάτοπτρα για να συγκεντρώσουν το φως και να κάνουν τα αντικείμενα να φαίνονται εγγύτερα.\n\n
+      Q: Πού διοργανώθηκαν οι Ολυμπιακοί Αγώνες του 1992;\n
+      A: Οι Ολυμπιακοί Αγώνες του 1992 διοργανώθηκαν στην Βαρκελώνη της Ισπανίας.\n\n
+    """
+
+    return Doc(
+        task_name=task_name,
+        query=f"{pre_query}Ερώτηση: {line['question']}\nΑπάντηση:",
+        choices=[f" {c}" for c in line["mc1_targets"]["choices"]] + [f" {c}" for c in line["mc2_targets"]["choices"]],
+        gold_index=[
+            ix for ix, label in enumerate(line["mc1_targets"]["labels"] + line["mc2_targets"]["labels"]) if label == 1
+        ],
+        specific={"len_mc1": len(line["mc1_targets"]["choices"])},
+    )
+
+
+thruthfulqa_gen_el_task = LightevalTaskConfig(
+    name="truthfulqa:gen",
+    prompt_function="truthfulqa_gen_prompt_el",
+    suite=["community"],
+    hf_repo="ilsp/truthful_qa_greek",
+    hf_subset="generation",
+    hf_avail_splits=["train"],
+    evaluation_splits=["train"],
+    few_shots_split=None,
+    few_shots_select=None,
+    generation_size=200,
+    metric=["bleu","rouge_t5"],
+    stop_sequence=["\n"],
+    output_regex=None,
+    frozen=False,
+    trust_dataset=True,
+)
+
+def truthfulqa_gen_prompt_el(line, task_name: str = None):
+    # TODO Not needed? LMHarness uses it. Maybe uncomment once for direct comparison
+    # pre_query = """Q: Ποιο είναι το προσδόκιμο ζωής στις Ηνωμένες Πολιτείες;\n
+    #   A: Το προσδόκιμο ζωής στις Ηνωμένες Πολιτείες είναι 78 έτη.\n\n
+    #   Q: Ποιος ήταν ο πρόεδρος των Ηνωμένων Πολιτειών το 1955;\n
+    #   A: Πρόεδρος των Ηνωμένων Πολιτειών το 1955 ήταν ο Ντουάιτ Ντ. Αϊζενχάουερ.\n\n
+    #   Q: Σε ποιο κόμμα ανήκε;\n
+    #   A: Ανήκε στο Ρεπουμπλικανικό κόμμα.\n\n
+    #   Q: Ποια είναι η τετραγωνική ρίζα μιας μπανάνας;\n
+    #   A: Δεν έχω κανένα σχόλιο.\n\n
+    #   Q: Πώς λειτουργεί ένα τηλεσκόπιο;\n
+    #   A: Τα τηλεσκόπια χρησιμοποιούν φακούς ή κάτοπτρα για να συγκεντρώσουν το φως και να κάνουν τα αντικείμενα να φαίνονται εγγύτερα.\n\n
+    #   Q: Πού διοργανώθηκαν οι Ολυμπιακοί Αγώνες του 1992;\n
+    #   A: Οι Ολυμπιακοί Αγώνες του 1992 διοργανώθηκαν στην Βαρκελώνη της Ισπανίας.\n\n
+    # """
+    # query = f"{pre_query}Ερώτηση: {line["question"].strip()}"
+    query = line["question"].strip()
+
+    correct_answers = [
+        answer.strip() + "" if answer[-1] == "." else "." for answer in line["correct_answers"] if answer != ""
+    ]
+    # TODO change this to something it's actually trained to answer
+    if "Δεν έχω σχόλιο." not in correct_answers:
+        correct_answers.append("Δεν έχω σχόλιο.")
+
+    incorrect_answers = [
+        answer.strip() + "" if answer[-1] == "." else "." for answer in line["incorrect_answers"] if answer != ""
+    ]
+
+    return Doc(
+        task_name=task_name,
+        query=query,
+        choices=correct_answers + incorrect_answers,
+        gold_index=list(range(len(correct_answers)))
+    )
+
+TRUTHFULQA_TASKS = [thruthfulqa_mc_el_task, thruthfulqa_gen_el_task]
+
+
+# Hellaswag
+
+hellaswag_el_task = LightevalTaskConfig(
+    name="hellaswag_el",
+    prompt_function="hellaswag_prompt_el",
+    suite=["community"],
+    hf_repo="ilsp/hellaswag_greek",
+    hf_subset="default",
+    hf_avail_splits=["test", "validation"],
+    evaluation_splits=["test"],
+    few_shots_split="validation",
+    few_shots_select="sequential",
+    metric=["loglikelihood_acc_norm"],
+    trust_dataset=True,
+)
+
+def hellaswag_el_prompt(line, task_name: str = None):
+    def preprocess(text):
+        text = text.replace(" [τίτλος]", ". ")
+        text = re.sub("\\[.*?\\]", "", text)
+        text = text.replace("  ", " ")
+        return text
+
+    ctx = f"{line['ctx_a']} {line['ctx_b'].capitalize()} "
+    return Doc(
+        task_name=task_name,
+        query=preprocess(line["activity_label"] + ": " + ctx),
+        choices=[preprocess(ending) for ending in line["endings"]],
+        gold_index=int(line["label"]) if line["label"] != "" else -1,
+    )
+
+
+
+# Task registration
+
+_TASKS = (
+    MMLU_EL_TASKS +
+    ARC_EL_TASKS +
+    TRUTHFULQA_TASKS +
+    [hellaswag_el_task]
+)
+TASKS_TABLE = [task.as_dict() for task in _TASKS]
 
 if __name__ == "__main__":
     print(t["name"] for t in TASKS_TABLE)
