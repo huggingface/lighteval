c4f9bfe7d715522b136675db9bb97aeef8459bb4
PhilipMay <philip@may.la>
2024-04-07 22:27:09 +0200

    add own copyright

--- community_tasks/ger_rag_eval.py
+++ community_tasks/ger_rag_eval.py
@@ -1,6 +1,7 @@
 # MIT License
 
 # Copyright (c) 2024 The HuggingFace Team
+# Copyright (c) 2024 Philip May, Deutsche Telekom AG
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
