7f6f8077f36e255a1e0c1d71531b0d5302a074cf
Nathan Habib <30601243+NathanHB@users.noreply.github.com>
2024-04-11 13:44:14 +0200

    Add llm as judge in metrics (#146)
    Now allows launching an Open AI LLM as a judge simply in the metrics.

--- src/lighteval/tasks/extended/mt_bench/main.py
+++ src/lighteval/tasks/extended/mt_bench/main.py
@@ -26,7 +26,6 @@ import numpy as np
 from aenum import extend_enum
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-from lighteval.tasks.extended.mt_bench.judges import JudgeOpenAI
 from lighteval.metrics import Metrics
 from lighteval.metrics.utils import MetricCategory, MetricUseCase, SampleLevelMetric, SampleLevelMetricGrouping
 from lighteval.tasks.lighteval_task import LightevalTaskConfig
@@ -35,15 +34,6 @@ from lighteval.tasks.tasks_prompt_formatting import LETTER_INDICES
 from colorama import Fore, Style
 import os
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-
-if OPENAI_API_KEY is None:
-    # Using print here because hlog_warn is not yet available in this context
-    print(
-        Fore.YELLOW
-        + "No OpenAI API key found. If you are using the OpenAI judge, please set the OPENAI_API_KEY environment variable."
-        + Style.RESET_ALL
-    )
 
 task = LightevalTaskConfig(
     name="mt_bench",
@@ -55,7 +45,7 @@ task = LightevalTaskConfig(
     evaluation_splits=["train"],
     few_shots_split="",
     few_shots_select="random",
-    metric=["mt_bench_metric"],
+    metric=["llm_judge_multi_turn"],
     generation_size=1024,
     stop_sequence=[],
 )
@@ -81,54 +71,9 @@ def prompt_fn(line, task_name: str = None):
     )
 
 
-def mt_bench_metric(predictions: list[str], formatted_doc: Doc, **kwargs) -> dict[str, float]:
-    """Defines how to go from a list of predictions to a score.
-    Follow examples in src/lighteval/metrics/metrics.py, or get more info
-    about what this function should do in the README.
-    """
-
-    judge = JudgeOpenAI(
-        model="gpt-3.5-turbo",
-        seed=42,
-        temperature=0.0,
-        templates_path="src/lighteval/tasks/extended/mt_bench/judge_prompts.jsonl",
-        openai_api_key=OPENAI_API_KEY,
-    )
-
-    questions = formatted_doc.specific["multi_turn_queries"]
-    ref_answers = formatted_doc.specific["reference"]
-
-    score, messages, judgement = judge.evaluate_answer(questions, predictions, ref_answers, single_turn=True)
-    score_mt, messages_mt, judgement_mt = judge.evaluate_answer(questions, predictions, ref_answers, single_turn=False)
-
-    return {
-        "single_turn": score,
-        "multi_turn": score_mt,
-        "user_prompt": [messages, messages_mt],
-        "judgement": [judgement, judgement_mt],
-    }
-
-
-mt_bench_metric = SampleLevelMetricGrouping(
-    metric="mt_bench_metric",
-    higher_is_better=True,
-    category=MetricCategory.GENERATIVE_MULTI_TURN,
-    use_case=MetricUseCase.SUMMARIZATION,
-    sample_level_fn=mt_bench_metric,
-    corpus_level_fn={
-        "single_turn": np.mean,
-        "multi_turn": np.mean,
-    },
-)
-
 _TASKS = [task]
 
 TASKS_TABLE = [task.as_dict() for task in _TASKS]
-extend_enum(
-    Metrics,
-    "mt_bench_metric",
-    mt_bench_metric,
-)
 
 if __name__ == "__main__":
     print(t["name"] for t in TASKS_TABLE)
