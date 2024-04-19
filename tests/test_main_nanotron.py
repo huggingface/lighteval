d07ca91e743de3b5f8bafc9bc9d4335a1454f03d
zzhhjjj <z785566960@gmail.com>
2024-04-19 10:05:25 +0000

    remove useless code

--- tests/test_main_nanotron.py
+++ tests/test_main_nanotron.py
@@ -76,52 +76,6 @@ def run_model_predictions_lite(config_path: str, lighteval_config_path: str):
     return results
 
 
-# def pytest_generate_tests(metafunc: pytest.Metafunc):
-#     """Initializes the main test setup. This function is automatically called by pytest and
-#     should not be called manually.
-
-#     Every function with "model_input" as arguments will be sent the "parameters".
-#     This function will be run only once, ensuring that each model is run only once on the selected tasks.
-#     (This is better than using fixtures as fixtures are re-run once for each test, which is not a behavior we want).
-#     """
-#     parameters = []
-
-#     # If model_input is a test function argument
-#     # (= the function requires a fixture)
-#     if "model_input" in metafunc.fixturenames:
-#         # tasks = TASKS  # must be a list not a file name
-#         for model in MODELS:
-#             if FULL_TEST:
-#                 predictions_full = run_model_predictions_full(model["config_path"], LIGHTEVAL_CONFIG_PATH)
-#                 # store the results
-#                 if SAVE_RESULTS:
-#                     with open(f"{RESULTS_DIRECTORY}/predictions_full.json", "w") as file:
-#                         json.dump(predictions_full["results"], file, indent=4)
-
-#                 tasks = list(RESULTS_NANOTRON_FULL[model["name"]].keys())
-#                 for eval_name in tasks:
-#                     for metric, reference in RESULTS_NANOTRON_FULL[model["name"]][eval_name].items():
-#                         if len(eval_name.split("|")) == 4:
-#                             eval_name = "|".join(eval_name.split("|")[:-1])
-#                         prediction = predictions_full["results"][eval_name.replace("|", ":")][metric]
-#                         parameters.append((model, "all", eval_name, metric, prediction, reference))
-#             else:
-#                 predictions_lite = run_model_predictions_lite(model["config_path"], LIGHTEVAL_CONFIG_PATH)
-#                 # store the results
-#                 if SAVE_RESULTS:
-#                     with open(f"{RESULTS_DIRECTORY}/predictions_lite.json", "w") as file:
-#                         json.dump(predictions_lite["results"], file, indent=4)
-
-#                 tasks = list(RESULTS_NANOTRON_LITE[model["name"]].keys())
-#                 for eval_name in tasks:
-#                     for metric, reference in RESULTS_NANOTRON_LITE[model["name"]][eval_name].items():
-#                         if len(eval_name.split("|")) == 4:
-#                             eval_name = "|".join(eval_name.split("|")[:-1])
-#                         prediction = predictions_lite["results"][eval_name.replace("|", ":")][metric]
-#                         parameters.append((model, "lite", eval_name, metric, prediction, reference))
-#         metafunc.parametrize("model_input", parameters, scope="session")
-
-
 def generate_full_test_parameters(model, tasks, results_nanotron_full):
     predictions_full = run_model_predictions_full(model["config_path"], LIGHTEVAL_CONFIG_PATH)
     if SAVE_RESULTS:
