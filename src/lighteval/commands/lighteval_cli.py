16fe94efef51e09d619bd8fea70e9eb8bc54810f
Nathan Habib <nathan.habib@huggingface.co>
2024-04-22 12:04:39 +0000

    fix tests

--- src/lighteval/commands/lighteval_cli.py
+++ src/lighteval/commands/lighteval_cli.py
@@ -29,78 +29,7 @@ from pprint import pprint
 from accelerate.commands.launch import launch_command, launch_command_parser  # noqa: I001
 
 from lighteval.commands.utils import list_tasks_command
-
-
-TOKEN = os.getenv("HF_TOKEN")
-CACHE_DIR = os.getenv("HF_HOME")
-
-def parser_accelerate(parser):
-    group = parser.add_mutually_exclusive_group(required=True)
-    task_type_group = parser.add_mutually_exclusive_group(required=True)
-
-    # Model type: either use a config file or simply the model name
-    task_type_group.add_argument("--model_config_path")
-    task_type_group.add_argument("--model_args")
-
-
-    parser.add_argument("--num_processes", type=int, default=1)
-
-    # Debug
-    parser.add_argument("--max_samples", type=int, default=None)
-    parser.add_argument("--override_batch_size", type=int, default=-1)
-    parser.add_argument("--job_id", type=str, help="Optional Job ID for future reference", default="")
-    # Saving
-    parser.add_argument("--output_dir", required=True)
-    parser.add_argument("--push_results_to_hub", default=False, action="store_true")
-    parser.add_argument("--save_details", action="store_true")
-    parser.add_argument("--push_details_to_hub", default=False, action="store_true")
-    parser.add_argument(
-        "--public_run", default=False, action="store_true", help="Push results and details to a public repo"
-    )
-    parser.add_argument("--cache_dir", type=str, default=CACHE_DIR)
-    parser.add_argument(
-        "--results_org",
-        type=str,
-        help="Hub organisation where you want to store the results. Your current token must have write access to it",
-    )
-    # Common parameters
-    parser.add_argument("--use_chat_template", default=False, action="store_true")
-    parser.add_argument("--system_prompt", type=str, default=None)
-    parser.add_argument("--dataset_loading_processes", type=int, default=1)
-    parser.add_argument(
-        "--custom_tasks",
-        type=str,
-        default=None,
-        help="Path to a file with custom tasks (a TASK list of dict and potentially prompt formating functions)",
-    )
-    group.add_argument(
-        "--tasks",
-        type=str,
-        default=None,
-        help="Id of a task, e.g. 'original|mmlu:abstract_algebra|5' or path to a texte file with a list of tasks",
-    )
-    parser.add_argument("--num_fewshot_seeds", type=int, default=1, help="Number of trials the few shots")
-    return parser
-
-
-def parser_nanotron(parser):
-    parser.add_argument(
-        "--checkpoint-config-path",
-        type=str,
-        required=True,
-        help="Path to the brr checkpoint YAML or python config file, potentially on S3",
-    )
-    parser.add_argument(
-        "--lighteval-override",
-        type=str,
-        help="Path to an optional YAML or python Lighteval config to override part of the checkpoint Lighteval config",
-    )
-    parser.add_argument(
-        "--cache-dir",
-        type=str,
-        default=None,
-        help="Cache directory",
-    )
+from lighteval.commands.parsers import parser_accelerate, parser_nanotron
 
 
 def main():
@@ -116,7 +45,7 @@ def main():
     parser_b = subparsers.add_parser('nanotron', help='use nanotron as backend for evaluation.')
     parser_nanotron(parser_b)
 
-    parser_c = subparsers.add_parser('list-tasks', help='List available tasks')
+    subparsers.add_parser('list-tasks', help='List available tasks')
 
     args = parser.parse_args()
 
