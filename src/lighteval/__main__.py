fa82b130c6f80ce5282326240f2188cc09d6737b
Nathan Habib <nathan.habib@huggingface.co>
2024-04-30 09:55:34 +0000

    fix nanotron launcher

--- src/lighteval/__main__.py
+++ src/lighteval/__main__.py
@@ -53,7 +53,7 @@ def cli_evaluate():
     if args.subcommand == "nanotron":
         from lighteval.main_nanotron import main as main_nanotron
 
-        main_nanotron(args)
+        main_nanotron(args.checkpoint_config_path, args.lighteval_override, args.cache_dir)
         return
 
     if args.list_tasks:
