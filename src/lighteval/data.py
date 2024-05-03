9b35ff41a61152b460ec805c2d3172ab4c7c5af4
Clémentine Fourrier <22726840+clefourrier@users.noreply.github.com>
2024-05-03 11:27:47 +0200

    Update src/lighteval/data.py
    Co-authored-by: Nathan Habib <30601243+NathanHB@users.noreply.github.com>

--- src/lighteval/data.py
+++ src/lighteval/data.py
@@ -232,7 +232,7 @@ class GenerativeTaskDataset(DynamicBatchDataset):
         splits_indices[-1][1] = self.total_size
 
         num_dataset_splits = len(splits_indices)
-
+    split_indices = [tuple(e) for e in split_indices]
         return num_dataset_splits, splits_indices
 
     def _sorting_criteria(self, request: GreedyUntilRequest) -> int:
