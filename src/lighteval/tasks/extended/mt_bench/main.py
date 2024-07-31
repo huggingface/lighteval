5df8df31eb569f313c52bb5a3224bd46358798d9
Nathan Habib <nathan.habib@huggingface.co>
2024-07-31 23:42:16 +0000

    fix tests

--- src/lighteval/tasks/extended/mt_bench/main.py
+++ src/lighteval/tasks/extended/mt_bench/main.py
@@ -56,7 +56,7 @@ task = LightevalTaskConfig(
     evaluation_splits=["train"],
     few_shots_split="",
     few_shots_select="random",
-    metric=[Metrics.llm_judge_multi_turn_llama_gpt3p5],
+    metric=[Metrics.llm_judge_multi_turn_gpt3p5],
     generation_size=1024,
     stop_sequence=[],
 )
