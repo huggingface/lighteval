8d4ed6f197c24fcbed8c385a7fc1a100beab229e
Clémentine Fourrier <22726840+clefourrier@users.noreply.github.com>
2024-08-01 10:43:45 +0200

    Expose samples via the CLI (#228)
    * add examples of samples (does not include few shot), and add robustness to logger
    
    * added cache at all levels
    
    * added nice printing for the config and more options for task display
    
    * added pprint
    
    ---------
    
    Co-authored-by: Nathan Habib <30601243+NathanHB@users.noreply.github.com>

--- src/lighteval/__main__.py
+++ src/lighteval/__main__.py
@@ -23,24 +23,32 @@
 # SOFTWARE.
 
 import argparse
+import os
+from dataclasses import asdict
+from pprint import pformat
 
-from lighteval.parsers import parser_accelerate, parser_nanotron
-from lighteval.tasks.registry import Registry
+from lighteval.parsers import parser_accelerate, parser_nanotron, parser_utils_tasks
+from lighteval.tasks.registry import Registry, taskinfo_selector
+
+
+CACHE_DIR = os.getenv("HF_HOME")
 
 
 def cli_evaluate():
     parser = argparse.ArgumentParser(description="CLI tool for lighteval, a lightweight framework for LLM evaluation")
     subparsers = parser.add_subparsers(help="help for subcommand", dest="subcommand")
 
-    # create the parser for the "accelerate" command
+    # Subparser for the "accelerate" command
     parser_a = subparsers.add_parser("accelerate", help="use accelerate and transformers as backend for evaluation.")
     parser_accelerate(parser_a)
 
-    # create the parser for the "nanotron" command
+    # Subparser for the "nanotron" command
     parser_b = subparsers.add_parser("nanotron", help="use nanotron as backend for evaluation.")
     parser_nanotron(parser_b)
 
-    parser.add_argument("--list-tasks", action="store_true", help="List available tasks")
+    # Subparser for task utils functions
+    parser_c = subparsers.add_parser("tasks", help="use nanotron as backend for evaluation.")
+    parser_utils_tasks(parser_c)
 
     args = parser.parse_args()
 
@@ -48,17 +56,37 @@ def cli_evaluate():
         from lighteval.main_accelerate import main as main_accelerate
 
         main_accelerate(args)
-        return
 
-    if args.subcommand == "nanotron":
+    elif args.subcommand == "nanotron":
         from lighteval.main_nanotron import main as main_nanotron
 
         main_nanotron(args.checkpoint_config_path, args.lighteval_override, args.cache_dir)
-        return
 
-    if args.list_tasks:
-        Registry(cache_dir="").print_all_tasks()
-        return
+    elif args.subcommand == "tasks":
+        if args.list:
+            Registry(cache_dir="").print_all_tasks()
+
+        if args.inspect:
+            print(f"Loading the tasks dataset to cache folder: {args.cache_dir}")
+            print(
+                "All examples will be displayed without few shot, as few shot sample construction requires loading a model and using its tokenizer."
+            )
+            # Loading task
+            task_names_list, _ = taskinfo_selector(args.inspect)
+            task_dict = Registry(cache_dir=args.cache_dir).get_task_dict(task_names_list)
+            for name, task in task_dict.items():
+                print("-" * 10, name, "-" * 10)
+                if args.show_config:
+                    print("-" * 10, "CONFIG")
+                    task.print_config()
+                for ix, sample in enumerate(task.eval_docs()[: int(args.num_samples)]):
+                    if ix == 0:
+                        print("-" * 10, "SAMPLES")
+                    print(f"-- sample {ix} --")
+                    print(pformat(asdict(sample), indent=1))
+
+    else:
+        print("You did not provide any argument. Exiting")
 
 
 if __name__ == "__main__":
