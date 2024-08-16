6f917036df8ae1e939162d3f31286d9f0031647f
Clémentine Fourrier <22726840+clefourrier@users.noreply.github.com>
2024-08-16 20:46:38 +0200

    Update __main__.py

--- src/lighteval/__main__.py
+++ src/lighteval/__main__.py
@@ -69,7 +69,7 @@ def cli_evaluate():
         if args.inspect:
             print(f"Loading the tasks dataset to cache folder: {args.cache_dir}")
             print(
-                "All examples will be displayed without few shot, as few shot sample construction requires loading a model and using its tokenizer."
+                "All examples will be displayed without few shot, as few shot sample construction requires loading a model and using its tokenizer. "
             )
             # Loading task
             task_names_list, _ = taskinfo_selector(args.inspect)
