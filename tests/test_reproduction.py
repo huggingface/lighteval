808e6898e8bc57df999618da9342051b1012f529
Sadra Barikbin <sadraqazvin1@yahoo.com>
2024-09-03 19:59:04 +0330

    Fix formatting

--- tests/test_reproduction.py
+++ tests/test_reproduction.py
@@ -1,10 +1,32 @@
+# MIT License
+
+# Copyright (c) 2024 The HuggingFace Team
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from lighteval.evaluator import EvaluationTracker, evaluate
+from lighteval.metrics.metrics import Metrics
+from lighteval.models.base_model import BaseModelConfig
 from lighteval.models.model_config import EnvConfig
 from lighteval.models.model_loader import load_model
-from lighteval.evaluator import EvaluationTracker, evaluate
-from lighteval.models.base_model import BaseModelConfig
 from lighteval.tasks.lighteval_task import LightevalTask, LightevalTaskConfig
-from lighteval.tasks.requests import Doc, TaskExampleId, RequestType
-from lighteval.metrics.metrics import Metrics
+from lighteval.tasks.requests import Doc, RequestType, TaskExampleId
 from lighteval.tasks.tasks_prompt_formatting import arc
 
 
