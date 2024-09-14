788c7250957db676122a22b784fb0757e37cfe66
Nick Doiron <ndoiron@mapmeld.com>
2024-09-13 22:27:49 -0500

    allow AdapterModel to read ModelConfig.tokenizer

--- src/lighteval/models/adapter_model.py
+++ src/lighteval/models/adapter_model.py
@@ -41,7 +41,7 @@ class AdapterModel(BaseModel):
     def _create_auto_tokenizer(self, config: AdapterModelConfig, env_config: EnvConfig) -> PreTrainedTokenizer:
         # By default, we look at the model config for the model stored in `base_model`
         # (= the parent model, not the model of interest)
-        return self._create_auto_tokenizer_with_name(config.base_model, config=config, env_config=env_config)
+        return self._create_auto_tokenizer_with_name(config.tokenizer or config.base_model, config=config, env_config=env_config)
 
     def _create_auto_model(self, config: AdapterModelConfig, env_config: EnvConfig) -> AutoModelForCausalLM:
         """Returns a PeftModel from a base model and a version fined tuned using PEFT."""
