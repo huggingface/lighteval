Fix BLEURT score computation errors due to incompatible input args and types.
Detailed issue description: https://github.com/huggingface/lighteval/issues/315

src/lighteval/metrics/metrics.py
@@ -23,6 +23,7 @@
 import os
 
 import numpy as np
+import torch
 from aenum import Enum
 
 from lighteval.metrics.harness_compatibility.drop import drop_metrics
@@ -123,12 +124,14 @@ class Metrics(Enum):
         corpus_level_fn=np.mean,
         higher_is_better=True,
     )
+    def compute_mean(x):
+        return np.mean(np.asarray(x).flatten()) # flatten, then average
     bleurt = SampleLevelMetric(
         metric_name="bleurt",
-        sample_level_fn=BLEURT.compute,
+        sample_level_fn=BLEURT().compute,
         category=MetricCategory.GENERATIVE,
         use_case=MetricUseCase.TRANSLATION,
-        corpus_level_fn=lambda x: np.mean(x.flatten()),  # flatten, then average
+        corpus_level_fn = compute_mean,
         higher_is_better=True,
     )
     byte_perplexity = CorpusLevelMetric(