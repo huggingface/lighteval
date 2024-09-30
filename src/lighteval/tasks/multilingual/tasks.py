ca865bd7e3825f450bd2f3ec54e3e251b69b6891
Hynek Kydlíček <kydlicek.hynek@gmail.com>
2024-09-30 18:21:43 +0200

    Update src/lighteval/tasks/multilingual/tasks.py
    Co-authored-by: Clémentine Fourrier <22726840+clefourrier@users.noreply.github.com>

--- src/lighteval/tasks/multilingual/tasks.py
+++ src/lighteval/tasks/multilingual/tasks.py
@@ -47,7 +47,7 @@ xnli_tasks = [
             adapter=lambda line: {
                 "premise": line["premise"],
                 "hypothesis": line["hypothesis"],
-                # Since we ignore the neural label
+                # Since we ignore the neutral label
                 "gold_idx": {0: 0, 2: 1}[line["label"]],
             },
             relations=["entailment", "contradiction"],
