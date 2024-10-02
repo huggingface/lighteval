6c0615207e1db87f1b96ff37b1d198c223ffe4af
Hynek Kydlíček <kydlicek.hynek@gmail.com>
2024-10-02 17:00:33 +0200

    Multilingual General Knowledge tasks (#338)
    * add multilignaul dynamic generative metrics
    
    * draft
    
    * finish multichoice config
    
    * update tokenizers + install nltk reqs
    
    * use punkt tab
    
    * Update src/lighteval/utils/imports.py
    
    Co-authored-by: Nathan Habib <30601243+NathanHB@users.noreply.github.com>
    
    * Update src/lighteval/metrics/normalizations.py
    
    Co-authored-by: Nathan Habib <30601243+NathanHB@users.noreply.github.com>
    
    * fix imports
    
    * remove unused import
    
    * finish implementation of templates + move stuff around
    
    * resolve nits
    
    * when in rome do as romans do (handle error messages the same way)
    
    * fix utils
    
    * nicers tests + fix them
    
    * nicer todo
    
    * add nice doscrings 📃
    
    * add even more docstring
    
    * nit
    
    * fix test
    
    * add multilingual to dev group
    
    * merge nli, add languagees to literals
    
    * translation literals
    
    * add nli
    
    * add copa tasks + fix tranlation literals
    
    * add hellaswag tasks
    
    * remove custom telgu hellaswag
    
    * remove hindi hellaswag
    
    * add rc tasks + small nits
    
    * add rcb + chinese nli
    
    * add mcq tasks
    
    * Update src/lighteval/tasks/multilingual/tasks.py
    
    Co-authored-by: Clémentine Fourrier <22726840+clefourrier@users.noreply.github.com>
    
    * Update src/lighteval/tasks/multilingual/tasks.py
    
    Co-authored-by: Clémentine Fourrier <22726840+clefourrier@users.noreply.github.com>
    
    * Update src/lighteval/tasks/multilingual/tasks.py
    
    Co-authored-by: Clémentine Fourrier <22726840+clefourrier@users.noreply.github.com>
    
    * Update src/lighteval/tasks/multilingual/tasks.py
    
    Co-authored-by: Clémentine Fourrier <22726840+clefourrier@users.noreply.github.com>
    
    * Update src/lighteval/tasks/multilingual/tasks.py
    
    Co-authored-by: Clémentine Fourrier <22726840+clefourrier@users.noreply.github.com>
    
    * Update src/lighteval/tasks/multilingual/tasks.py
    
    Co-authored-by: Clémentine Fourrier <22726840+clefourrier@users.noreply.github.com>
    
    * Update src/lighteval/tasks/multilingual/tasks.py
    
    Co-authored-by: Clémentine Fourrier <22726840+clefourrier@users.noreply.github.com>
    
    * add two new tasks + docs
    
    * add nice docs
    
    * update hellaswag with docs
    
    * move hellaswag to lighteval suite
    
    * add desc to tasks
    
    * Update src/lighteval/tasks/multilingual/tasks.py
    
    Co-authored-by: Clémentine Fourrier <22726840+clefourrier@users.noreply.github.com>
    
    * enable returning none from templates + better typing
    
    * add nice docs
    
    * fix saving of partial fcs + better tasks doc
    
    * change unoficial hellaswag names to have community_prefix + unify hellaswag preprocesisng
    
    * community rename
    
    * let strip be optional in hellaswag
    
    * nits
    
    * fix remaning tasks in mcq
    
    * fixes
    
    * nits
    
    * fix up agieval + ceval
    
    * add comment
    
    * update the datasets after changing ownership
    
    * translation literals defaults + nits in adapters
    
    * Empty commit to retrigger actions
    
    ---------
    
    Co-authored-by: Nathan Habib <30601243+NathanHB@users.noreply.github.com>
    Co-authored-by: Hynek Kydlicek <kydliceh.hynek@gmail.com>
    Co-authored-by: Clémentine Fourrier <22726840+clefourrier@users.noreply.github.com>

--- src/lighteval/tasks/templates/utils/translation_literals.py
+++ src/lighteval/tasks/templates/utils/translation_literals.py
@@ -49,14 +49,14 @@ class TranslationLiterals:
     neither: str = None  # type: ignore
 
     # Punctuation
-    full_stop: str = None  # type: ignore
-    comma: str = None  # type: ignore
-    question_mark: str = None  # type: ignore
-    exclamation_mark: str = None  # type: ignore
-    word_space: str = None  # type: ignore
-    sentence_space: str = None  # type: ignore
-    colon: str = ":"  # type: ignore
-    semicolon: str = ";"  # type: ignore
+    full_stop: str = "."
+    comma: str = ","
+    question_mark: str = "?"
+    exclamation_mark: str = "!"
+    word_space: str = " "
+    sentence_space: str = " "
+    colon: str = ":"
+    semicolon: str = ";"
 
     # Indices
     indices: list[str] = field(default_factory=lambda: LETTER_INDICES)
