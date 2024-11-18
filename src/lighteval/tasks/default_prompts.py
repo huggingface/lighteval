0c651c01dd124ded3c32a16cae49c4899f7a1b23
clementine@huggingface.co <clementine@huggingface.co>
2024-11-18 12:23:57 +0000

    updated tests

--- src/lighteval/tasks/default_prompts.py
+++ src/lighteval/tasks/default_prompts.py
@@ -1625,13 +1625,13 @@ def mmlu_helm(line, task_name: str = None):
     query += "\nAnswer:"
 
     gold_ix = LETTER_INDICES.index(line["answer"]) if isinstance(line["answer"], str) else line["answer"]
-    is_few_shots = line.get("__few_shots", False)  # We are adding few shots
 
     return Doc(
         task_name=task_name,
         query=query,
-        choices=[" A", " B", " C", " D"] if not is_few_shots else line["choices"],
+        choices=[" A", " B", " C", " D"],
         gold_index=gold_ix,
+        fewshot_sorting_class=line["choices"][gold_ix],
         instruction=f"The following are multiple choice questions (with answers) about {subject.replace('_', ' ')}.\n\n",
     )
 
