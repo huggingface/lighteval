c2337cfdf38fae54e160b6fa4296e8151842bfb2
Nick Doiron <ndoiron@mapmeld.com>
2024-12-12 05:45:35 -0500

    Allow AdapterModels to have custom tokens (#306)
    ---------
    
    Co-authored-by: Clémentine Fourrier <22726840+clefourrier@users.noreply.github.com>

--- src/lighteval/models/transformers/adapter_model.py
+++ src/lighteval/models/transformers/adapter_model.py
@@ -84,6 +84,18 @@ class AdapterModel(BaseModel):
             base = AutoModelForCausalLM.from_pretrained(
                 config.base_model, torch_dtype=torch.float16, low_cpu_mem_usage=True, token=env_config.token
             )
+            # resize model for adapters with added tokens
+            token_diff = len(self._tokenizer) - base.config.vocab_size
+            if token_diff != 0:
+                if token_diff > 0:
+                    logger.info(
+                        f"You're using the adapter model's tokenizer, which has more tokens than the base model. Adding {token_diff} token(s)."
+                    )
+                else:
+                    logger.info(
+                        f"You're using the adapter model's tokenizer, which has fewer tokens than the base model. Removing {abs(token_diff)} token(s)."
+                    )
+                base.resize_token_embeddings(len(self._tokenizer))
             # Should pass revision
             model = PeftModel.from_pretrained(base, adapter_weights)
             model = model.merge_and_unload()
