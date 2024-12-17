6ed58942ec33f61f9b099e3a8f599bf1860d7420
erikkaum <erik.kaum@gmail.com>
2024-12-17 12:01:16 +0100

    ruff style

--- src/lighteval/models/endpoints/endpoint_model.py
+++ src/lighteval/models/endpoints/endpoint_model.py
@@ -208,9 +208,7 @@ class InferenceEndpointModel(LightevalModel):
                                         **config.get_dtype_args(),
                                         **config.get_custom_env_vars(),
                                     },
-                                    "url": (
-                                        config.image_url or "ghcr.io/huggingface/text-generation-inference:3.0.1"
-                                    ),
+                                    "url": (config.image_url or "ghcr.io/huggingface/text-generation-inference:3.0.1"),
                                 },
                             )
                         else:  # Endpoint exists
