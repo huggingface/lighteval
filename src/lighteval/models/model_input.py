843b572f3f9f28d11741854496183dd4d4bf80da
Clémentine <cle.fourrier@gmail.com>
2024-12-26 12:33:12 +0100

    style

--- src/lighteval/models/model_input.py
+++ src/lighteval/models/model_input.py
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from dataclasses import dataclass, asdict
+from dataclasses import asdict, dataclass
 from typing import Optional
 
 
