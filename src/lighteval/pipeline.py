c4c264c9480670110356727a111f4a350a6b3371
T. Duy Nguyen-Hien <duynht@u.nus.edu>
2025-05-07 00:30:29 +0800

    used asdict instead of model_dump

--- src/lighteval/pipeline.py
+++ src/lighteval/pipeline.py
@@ -27,7 +27,7 @@ import random
 import re
 import shutil
 from contextlib import nullcontext
-from dataclasses import dataclass
+from dataclasses import asdict, dataclass
 from datetime import timedelta
 from enum import Enum, auto
 
@@ -154,8 +154,7 @@ class Pipeline:
         self._metric_options = metric_options or {}
         self.accelerator, self.parallel_context = self._init_parallelism_manager()
         self.model = self._init_model(model_config, model)
-
-        generation_parameters = model_config.generation_parameters.model_dump() if model_config and hasattr(model_config, "generation_parameters") else {}
+        generation_parameters = asdict(model_config.generation_parameters) if model_config and hasattr(model_config, "generation_parameters") else {}
 
         self.evaluation_tracker.general_config_logger.log_model_info(generation_parameters, self.model.model_info)
         self._init_random_seeds()
