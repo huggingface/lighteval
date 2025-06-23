68df230a6b8d4c5eab3db28d15b698fab4079566
Nathan Habib <nathan.habib@huggingface.co>
2025-06-23 12:33:56 +0000

    fix slow tests

--- tests/slow_tests/test_accelerate_model.py
+++ tests/slow_tests/test_accelerate_model.py
@@ -38,7 +38,6 @@ MODELS_ARGS = [
     # {"model_name": "gpt2", "use_chat_template": False, "revision": "main", "results_file": "tests/reference_scores/gpt2-results.json"},
     {
         "model_name": "examples/model_configs/transformers_model.yaml",
-        "use_chat_template": True,
         "results_file": "tests/reference_scores/SmolLM2-1.7B-Instruct-results-accelerate.json",
     }
 ]
@@ -49,12 +48,11 @@ ModelInput = Tuple[str, Callable[[], dict]]
 
 
 @lru_cache(maxsize=len(MODELS_ARGS))
-def run_model(model_name: str, use_chat_template: bool):
+def run_model(model_name: str):
     """Runs the full main as a black box, using the input model and tasks, on 10 samples without parallelism"""
     results = accelerate(
         model_args=model_name,
         tasks=TASKS_PATH,
-        use_chat_template=use_chat_template,
         output_dir="",
         dataset_loading_processes=1,
         save_details=False,
@@ -69,7 +67,7 @@ def generate_tests() -> list[ModelInput]:
 
     tests = []
     for model_args in MODELS_ARGS:
-        predictions_lite = partial(run_model, model_args["model_name"], model_args["use_chat_template"])
+        predictions_lite = partial(run_model, model_args["model_name"])
         tests.append((model_args, predictions_lite))
 
     return tests
