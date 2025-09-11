19048afaf8ea9f74254a1e8e016057b618983750
Junhao Li <junhao@ubicloud.com>
2025-09-10 18:00:44 -0700

    .

--- community_tasks/chart_qa_evals.py
+++ community_tasks/chart_qa_evals.py
@@ -23,13 +23,23 @@
 # ruff: noqa: F405, F403, F401
 """
 Task to evaluate VLMs on HuggingFaceM4/ChartQA.
+
+Example evaluation:
+lighteval accelerate "model_name=google/gemma-3-4b-it" "community|chart_qa|0" --custom-tasks community_tasks/chart_qa_evals.py --vision-model
 """
 
 import numpy as np
 
-from lighteval.metrics.metrics import Metrics
+from lighteval.metrics.dynamic_metrics import MultilingualExtractiveMatchMetric
+from lighteval.metrics.metrics import Metrics, SampleLevelMetric
+from lighteval.metrics.utils.extractive_match_utils import (
+    ExprExtractionConfig,
+    LatexExtractionConfig,
+)
+from lighteval.metrics.utils.metric_utils import SamplingMethod
 from lighteval.tasks.lighteval_task import LightevalTaskConfig
 from lighteval.tasks.requests import Doc
+from lighteval.utils.language import Language
 
 
 def prompt_fn(line, task_name: str = None):
@@ -39,12 +49,27 @@ def prompt_fn(line, task_name: str = None):
     """
     return Doc(
         task_name=task_name,
-        query="Answer the following question based on the chart. The last line of your response should be of the following format: 'Answer: $ANSWER' (without quotes) where $ANSWER is the answer to the question. Think step by step before answering.",
+        query="Answer the following question. The last line of your response should be of the following format: 'Answer: $ANSWER' (without quotes) where $ANSWER is the answer to the question.\n\n"
+        + line["query"],
         gold_index=0,
         choices=[line["label"]],
         images=[line["image"]],
     )
 
+
+extraction_targets = [ExprExtractionConfig(), LatexExtractionConfig()]
+metric = SampleLevelMetric(
+    metric_name="extractive_match",
+    sample_level_fn=MultilingualExtractiveMatchMetric(
+        language=Language.ENGLISH,
+        gold_extraction_target=extraction_targets,
+        pred_extraction_target=extraction_targets,
+        precision=6,
+    ),
+    category=SamplingMethod.GENERATIVE,
+    corpus_level_fn=np.mean,
+    higher_is_better=True,
+)
 
 task = LightevalTaskConfig(
     name="chart_qa",
@@ -52,11 +77,26 @@ task = LightevalTaskConfig(
     suite=["community"],
     hf_repo="HuggingFaceM4/ChartQA",
     hf_subset="default",
-    hf_avail_splits=["test"],
+    hf_avail_splits=["train", "val", "test"],
     evaluation_splits=["test"],
+    hf_filter=lambda line: line["human_or_machine"] == 0,
     few_shots_split=None,
     few_shots_select=None,
-    metrics=[Metrics.exact_match],  # select your metric in Metrics
+    metrics=[metric],  # select your metric in Metrics
 )
 
-TASKS_TABLE = [task]
+human_task = LightevalTaskConfig(
+    name="chart_qa:human",
+    prompt_function=prompt_fn,  # must be defined in the file or imported from src/lighteval/tasks/tasks_prompt_formatting.py
+    suite=["community"],
+    hf_repo="HuggingFaceM4/ChartQA",
+    hf_subset="default",
+    hf_avail_splits=["train", "val", "test"],
+    evaluation_splits=["test"],
+    hf_filter=lambda line: line["human_or_machine"] == 0,
+    few_shots_split=None,
+    few_shots_select=None,
+    metrics=[metric],  # select your metric in Metrics
+)
+
+TASKS_TABLE = [task, human_task]
