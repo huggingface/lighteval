b661d0d77aea6f2c462c53864477c3a15b422bca
Nathan Habib <nathan.habib19@gmail.com>
2025-11-03 16:32:12 +0100

    better help for eval

--- src/lighteval/main_inspect.py
+++ src/lighteval/main_inspect.py
@@ -31,6 +31,8 @@ from inspect_ai.dataset import hf_dataset
 from inspect_ai.scorer import exact
 from inspect_ai.solver import generate, system_message
 from pytablewriter import MarkdownTableWriter
+from typer import Argument, Option
+from typing_extensions import Annotated
 
 from lighteval.models.abstract_model import InspectAIModelConfig
 from lighteval.tasks.lighteval_task import LightevalTaskConfig
@@ -81,9 +83,9 @@ def get_inspect_ai_task(
     return Task(dataset=dataset, solver=solver, scorer=scorers, name=name, epochs=Epochs(epochs, epochs_reducer))
 
 
-def push_to_hub(bundle_dir: str, repo_id: str):
+def push_to_hub(bundle_dir: str, repo_id: str, public: bool = False):
     api = HfApi()
-    api.create_repo(repo_id=repo_id, repo_type="space", space_sdk="static", exist_ok=True)
+    api.create_repo(repo_id=repo_id, repo_type="space", space_sdk="static", exist_ok=True, private=not public)
     api.upload_folder(repo_id=repo_id, repo_type="space", folder_path=bundle_dir)
     print(f"Details pushed to https://huggingface.co/spaces/{repo_id}")
 
@@ -180,23 +182,212 @@ def _format_metric_cell(data: dict, col: str, metric: str, stderr_metric: str) -
     return "-"
 
 
+HELP_PANEL_NAME_1 = "Modeling Parameters"
+HELP_PANEL_NAME_2 = "Task Parameters"
+HELP_PANEL_NAME_3 = "Connection and parallelization parameters"
+HELP_PANEL_NAME_4 = "Logging parameters"
+
+
 def eval(
-    models: list[str],
-    tasks: str,
-    epochs: int = 1,
-    epochs_reducer: str | None = None,
-    max_connections: int = 50,
-    timeout: int = 30,
-    retry_on_error: int = 1,
-    max_retries: int = 5,
-    log_dir: str = "lighteval-logs",
-    log_dir_allow_dirty: bool = True,
-    display: Literal["rich", "full", "conversations", "plain", "log", "none"] = "rich",
-    model_config: str | None = None,
-    max_samples: int | None = None,
-    max_tasks: int | None = None,
-    bundle_dir: str | None = None,
-    repo_id: str | None = None,
+    models: Annotated[list[str], Argument(help="Models to evaluate")],
+    tasks: Annotated[str, Argument(help="Tasks to evaluate")],
+    # model arguments
+    max_tokens: Annotated[
+        int | None,
+        Option(
+            help="The maximum number of tokens that can be generated in the completion (default is model specific)",
+            rich_help_panel=HELP_PANEL_NAME_1,
+        ),
+    ] = None,
+    system_message: Annotated[
+        str | None,
+        Option(
+            help="System message to use, overrides the task defined system and default one.",
+            rich_help_panel=HELP_PANEL_NAME_1,
+        ),
+    ] = None,
+    temperature: Annotated[
+        float | None,
+        Option(
+            help="Controls randomness in the model's output. Lower values make the model more deterministic and focused, while higher values make it more creative and varied.",
+            rich_help_panel=HELP_PANEL_NAME_1,
+        ),
+    ] = None,
+    top_p: Annotated[
+        float | None,
+        Option(
+            help="An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass.",
+            rich_help_panel=HELP_PANEL_NAME_1,
+        ),
+    ] = None,
+    top_k: Annotated[
+        int | None,
+        Option(
+            help="The number of highest probability vocabulary tokens to keep for each step of decoding.",
+            rich_help_panel=HELP_PANEL_NAME_1,
+        ),
+    ] = None,
+    frequence_penalty: Annotated[
+        float | None,
+        Option(
+            help="Number between -2.0 and 2.0, Penalizes tokens that appear in the text too frequently, reducing repetition.",
+            rich_help_panel=HELP_PANEL_NAME_1,
+        ),
+    ] = None,
+    presence_penalty: Annotated[
+        float | None,
+        Option(
+            help="Number between -2.0 and 2.0, Penalizes tokens that appear in the text, increasing diversity of the generated text.",
+            rich_help_panel=HELP_PANEL_NAME_1,
+        ),
+    ] = None,
+    logit_bias: Annotated[
+        str | None,
+        Option(
+            help="Bias for each token, can be used to prioritize or deprioritize certain tokens, for example 10=100, -10=-100.",
+            rich_help_panel=HELP_PANEL_NAME_1,
+        ),
+    ] = None,
+    seed: Annotated[
+        int | None, Option(help="Random seed to use for reproducibility", rich_help_panel=HELP_PANEL_NAME_1)
+    ] = None,
+    stop_seqs: Annotated[
+        list[str] | None,
+        Option(
+            help="Stop sequences to use, can be used to stop the generation of the text.",
+            rich_help_panel=HELP_PANEL_NAME_1,
+        ),
+    ] = None,
+    num_choices: Annotated[
+        int | None,
+        Option(help="The number of choices to generate for each step of decoding.", rich_help_panel=HELP_PANEL_NAME_1),
+    ] = None,
+    best_of: Annotated[
+        int | None,
+        Option(
+            help="Generates best_of completions server-side and returns the one with the highest log probability per token.",
+            rich_help_panel=HELP_PANEL_NAME_1,
+        ),
+    ] = None,
+    log_probs: Annotated[
+        bool | None,
+        Option(
+            help="Returns log probabilities for each token in the generated text", rich_help_panel=HELP_PANEL_NAME_1
+        ),
+    ] = None,
+    top_logprobs: Annotated[
+        int | None,
+        Option(help="How many most likely tokens to return at each forward step.", rich_help_panel=HELP_PANEL_NAME_1),
+    ] = None,
+    cache_prompt: Annotated[
+        bool | None, Option(help="Cache prompt prefix.", rich_help_panel=HELP_PANEL_NAME_1)
+    ] = None,
+    reasoning_effort: Annotated[
+        int | None, Option(help="Value: `minimal`, `low`, `medium`, `high`", rich_help_panel=HELP_PANEL_NAME_1)
+    ] = None,
+    reasoning_tokens: Annotated[
+        int | None,
+        Option(help="Maximum number of tokens to generate for reasoning", rich_help_panel=HELP_PANEL_NAME_1),
+    ] = None,
+    reasoning_history: Annotated[
+        bool | None,
+        Option(
+            help="values: `none`, `all`, `last`, `auto`. Include reasoning in chat message history sent to generate (defaults to “auto”, which uses the recommended default for each provider)",
+            rich_help_panel=HELP_PANEL_NAME_1,
+        ),
+    ] = None,
+    response_format: Annotated[
+        str | None, Option(help="JSON schema for the response", rich_help_panel=HELP_PANEL_NAME_1)
+    ] = None,
+    parallel_tool_calls: Annotated[
+        bool | None, Option(help="Enable parallel tool calls", rich_help_panel=HELP_PANEL_NAME_1)
+    ] = None,
+    max_tool_output: Annotated[
+        int | None,
+        Option(help="Maximum number of tokens to generate for tool output", rich_help_panel=HELP_PANEL_NAME_1),
+    ] = None,
+    internal_tools: Annotated[
+        bool | None, Option(help="Enable internal tools", rich_help_panel=HELP_PANEL_NAME_1)
+    ] = None,
+    model_args: Annotated[
+        str | None, Option(help="Provider specific arguments: example: 'device=1'", rich_help_panel=HELP_PANEL_NAME_1)
+    ] = None,
+    # task parameters
+    custom_tasks: Annotated[
+        str | None,
+        Option(
+            help="Path to a Python file containing custom task definitions. The file should define a TASKS_TABLE with LightevalTaskConfig objects.",
+            rich_help_panel=HELP_PANEL_NAME_2,
+        ),
+    ] = None,
+    max_samples: Annotated[
+        int | None,
+        Option(
+            help="Maximum number of samples to use per task. If your task has multiple subtasks, this will be the maximum number of samples to use per subtask.",
+            rich_help_panel=HELP_PANEL_NAME_2,
+        ),
+    ] = None,
+    # Metric parameters
+    epochs: Annotated[
+        int,
+        Option(
+            help="Number of times to evaluate the model on the task, the results will be aggregated by the specified reducer.",
+            rich_help_panel=HELP_PANEL_NAME_2,
+        ),
+    ] = 1,
+    epochs_reducer: Annotated[
+        str | None,
+        Option(
+            help="Epochs Reducer to use: mean, median, mode, max, at_least_{n}, pass_at_{k}",
+            rich_help_panel=HELP_PANEL_NAME_2,
+        ),
+    ] = None,
+    # Connection and parallelization parameters
+    max_connections: Annotated[
+        int,
+        Option(
+            help="Maximum number of concurrent connections to use for each model", rich_help_panel=HELP_PANEL_NAME_3
+        ),
+    ] = 50,
+    timeout: Annotated[
+        int, Option(help="Timeout in seconds for each connection", rich_help_panel=HELP_PANEL_NAME_3)
+    ] = 30,
+    retry_on_error: Annotated[
+        int, Option(help="Number of times to retry on error", rich_help_panel=HELP_PANEL_NAME_3)
+    ] = 1,
+    max_retries: Annotated[
+        int, Option(help="Maximum number of retries to use", rich_help_panel=HELP_PANEL_NAME_3)
+    ] = 5,
+    max_tasks: Annotated[
+        int | None, Option(help="Maximum number of tasks to evaluate in parallel", rich_help_panel=HELP_PANEL_NAME_3)
+    ] = None,
+    # Logging parameters
+    log_dir: Annotated[
+        str,
+        Option(help="Log directory to use, will be created if it doesn't exist", rich_help_panel=HELP_PANEL_NAME_4),
+    ] = "lighteval-logs",
+    log_dir_allow_dirty: Annotated[
+        bool, Option(help="Allow dirty log directory", rich_help_panel=HELP_PANEL_NAME_4)
+    ] = True,
+    display: Annotated[
+        Literal["rich", "full", "conversations", "plain", "log", "none"],
+        Option(help="Display", rich_help_panel=HELP_PANEL_NAME_4),
+    ] = "rich",
+    bundle_dir: Annotated[
+        str | None,
+        Option(help="Bundle directory to use, will be created if it doesn't exist", rich_help_panel=HELP_PANEL_NAME_4),
+    ] = None,
+    repo_id: Annotated[
+        str | None,
+        Option(help="Repository ID to use, will be created if it doesn't exist", rich_help_panel=HELP_PANEL_NAME_4),
+    ] = None,
+    public: Annotated[
+        bool,
+        Option(
+            help="Whether to make the uploaded results and details public on the Hugging Face Hub. If False, datasets will be private.",
+            rich_help_panel=HELP_PANEL_NAME_4,
+        ),
+    ] = False,
 ):
     from lighteval.tasks.registry import Registry
 
@@ -208,12 +399,10 @@ def eval(
         for task_config in task_configs:
             inspect_ai_tasks.append(get_inspect_ai_task(task_config, epochs=epochs, epochs_reducer=epochs_reducer))
 
-    if model_config is not None and model_config.endswith(".yaml"):
-        model_config = InspectAIModelConfig.from_path(model_config).dict()
-    elif model_config is not None:
-        model_config = InspectAIModelConfig.from_args(model_config).dict()
+    if model_args is not None:
+        model_args = InspectAIModelConfig._parse_args(model_args)
     else:
-        model_config = {}
+        model_args = {}
 
     success, logs = inspect_ai_eval_set(
         inspect_ai_tasks,
@@ -228,7 +417,29 @@ def eval(
         log_dir_allow_dirty=log_dir_allow_dirty,
         display=display,
         bundle_dir=bundle_dir,
-        **model_config,
+        model_args=model_args,
+        max_tokens=max_tokens,
+        system_message=system_message,
+        temperature=temperature,
+        top_p=top_p,
+        top_k=top_k,
+        frequence_penalty=frequence_penalty,
+        presence_penalty=presence_penalty,
+        seed=seed,
+        stop_seqs=stop_seqs,
+        num_choices=num_choices,
+        best_of=best_of,
+        log_probs=log_probs,
+        top_logprobs=top_logprobs,
+        cache_prompt=cache_prompt,
+        reasoning_effort=reasoning_effort,
+        reasoning_tokens=reasoning_tokens,
+        reasoning_history=reasoning_history,
+        response_format=response_format,
+        parallel_tool_calls=parallel_tool_calls,
+        max_tool_output=max_tool_output,
+        internal_tools=internal_tools,
+        overwrite=True,
     )
 
     if not success:
@@ -247,7 +458,7 @@ def eval(
     table_md = results_to_markdown_table(results_per_model_per_task_agg)
 
     if repo_id is not None:
-        push_to_hub(bundle_dir, repo_id)
+        push_to_hub(bundle_dir, repo_id, public=public)
 
     print()
     print(table_md)
