012158eab9b57c2eedda2305b84c7b6f0b3b8432
Francesco Bertolotti <francesco.bertolotti@igenius.ai>
2025-11-24 17:26:29 +0100

    typ annotation fix

--- src/lighteval/utils/cache_management.py
+++ src/lighteval/utils/cache_management.py
@@ -63,7 +63,7 @@ def cached(sampling_method: None | SamplingMethod = None):  # noqa: C901
 
     def decorator(model_call: Callable):  # noqa: C901
         @functools.wraps(model_call)
-        def sync_wrapper(self, docs: List[Doc], *args, **kwargs):
+        def sync_wrapper(self, docs: Doc | List[Doc], *args, **kwargs):
             docs = as_list(docs)
             results = [None] * len(docs)
             with diskcache.Cache(self.config.cache_dir) as cache:
@@ -103,7 +103,7 @@ def cached(sampling_method: None | SamplingMethod = None):  # noqa: C901
             return results
 
         @functools.wraps(model_call)
-        async def async_wrapper(self, docs: List[Doc], *args, **kwargs):
+        async def async_wrapper(self, docs: Doc | List[Doc], *args, **kwargs):
             docs = as_list(docs)
             results = [None] * len(docs)
             with diskcache.Cache(self.config.cache_dir) as cache:
