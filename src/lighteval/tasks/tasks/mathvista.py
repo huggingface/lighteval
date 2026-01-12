b01aae6507602143b9aa6e2fb018ab0bd2513374
Nathan Habib <30601243+NathanHB@users.noreply.github.com>
2026-01-12 14:16:15 +0100

    Apply suggestion from @NathanHB

--- src/lighteval/tasks/tasks/mathvista.py
+++ src/lighteval/tasks/tasks/mathvista.py
@@ -51,7 +51,8 @@ from PIL import Image
 
 from lighteval.metrics.metrics import Metrics
 from lighteval.tasks.lighteval_task import LightevalTaskConfig
-code insperied by: https://github.com/UKGovernmentBEIS/inspect_evals/blob/main/src/inspect_evals/mathvista/mathvista.py
+
+# code insperied by: https://github.com/UKGovernmentBEIS/inspect_evals/blob/main/src/inspect_evals/mathvista/mathvista.py
 
 logger = logging.getLogger(__name__)
 
