77e69cfe21f46339c37b1bfab026929e1467ad30
mariabalueva <marybal7@yandex.ru>
2026-03-19 23:36:54 +0300

    add tests and second type of ttc

--- src/lighteval/metrics/custom_metrics.py
+++ src/lighteval/metrics/custom_metrics.py
@@ -2,19 +2,67 @@ from typing import Callable
 
 import numpy as np
 from aenum import extend_enum
-from transformers import GPT2Tokenizer
+from tqdm import tqdm
+from transformers import GPT2TokenizerFast
 
 from lighteval.metrics.metrics import Metrics
-from lighteval.metrics.metrics_sample import F1_score, SampleLevelComputation
+from lighteval.metrics.metrics_sample import ExactMatches, F1_score, SampleLevelComputation
 from lighteval.metrics.utils.metric_utils import SampleLevelMetric
 from lighteval.models.model_output import ModelResponse
 from lighteval.tasks.requests import Doc, SamplingMethod
 
 
-tokenizer = GPT2Tokenizer.from_pretrained("openai-community/gpt2")
+tokenizer = GPT2TokenizerFast.from_pretrained("openai-community/gpt2")
+f1 = F1_score()
+em = ExactMatches(type_exact_match="prefix", strip_strings=True)
+progress_bar = None
 
 
-class TTC_score(SampleLevelComputation):
+class TTC_score_F1(SampleLevelComputation):
+    def __init__(
+        self,
+        aggregation_function: Callable[[list[float]], float] = max,
+        normalize_gold: Callable[[str], str] | None = None,
+        normalize_pred: Callable[[str], str] | None = None,
+        strip_strings: bool = False,
+    ):
+        if aggregation_function is None:
+            aggregation_function = np.mean
+
+        self.aggregation_function = aggregation_function
+        self.normalize_gold = normalize_gold
+        self.normalize_pred = normalize_pred
+        self.strip_strings = strip_strings
+
+    def compute(self, doc: Doc, model_response: ModelResponse, **kwargs) -> float:
+        global progress_bar
+
+        if progress_bar is None:
+            progress_bar = tqdm(desc="Computing TTC metrics", unit="samples")
+
+        progress_bar.update(1)
+
+        response = model_response.final_text[0]
+        all_tokens = tokenizer.encode(response)
+        partial_model_response = ModelResponse(
+            text=[""],
+            input_tokens=model_response.input_tokens,
+            output_tokens=[],
+            logprobs=[],
+        )
+
+        for i in range(1, len(all_tokens) + 1):
+            partial_response = tokenizer.decode(all_tokens[:i])
+            partial_model_response.text = [partial_response]
+            partial_model_response.output_tokens = all_tokens[:i]
+            score = f1.compute(doc, partial_model_response)
+            if score >= 0.9:
+                return i / len(all_tokens)
+
+        return 1.0
+
+
+class TTC_score_EM(SampleLevelComputation):
     def __init__(
         self,
         aggregation_function: Callable[[list[float]], float] = max,
@@ -34,30 +82,42 @@ class TTC_score(SampleLevelComputation):
         response = model_response.final_text[0]
         all_tokens = tokenizer.encode(response)
 
+        partial_model_response = ModelResponse(
+            text=[""],
+            input_tokens=model_response.input_tokens,
+            output_tokens=[],
+            logprobs=[],
+        )
+
         for i in range(1, len(all_tokens) + 1):
             partial_response = tokenizer.decode(all_tokens[:i])
-            f1 = F1_score()
-            partial_model_response = ModelResponse(
-                text=[partial_response],
-                output_tokens=[],
-                logprobs=[],
-            )
-            score = f1.compute(doc, partial_model_response)
-            if score >= 0.9:
+            partial_model_response.text = [partial_response]
+            partial_model_response.output_tokens = all_tokens[:i]
+            score = em.compute(doc, partial_model_response)
+            if score >= 1.0:
                 return i / len(all_tokens)
 
         return 1.0
 
 
-my_custom_ttc_metric = SampleLevelMetric(
-    metric_name="ttc",
+my_custom_ttc_f1_metric = SampleLevelMetric(
+    metric_name="ttc_f1",
     higher_is_better=False,
     category=SamplingMethod.GENERATIVE,
-    sample_level_fn=TTC_score(),
+    sample_level_fn=TTC_score_F1(),
     corpus_level_fn=np.mean,
 )
 
-extend_enum(Metrics, "CUSTOM_TTC", my_custom_ttc_metric)
+my_custom_ttc_em_metric = SampleLevelMetric(
+    metric_name="ttc_em",
+    higher_is_better=False,
+    category=SamplingMethod.GENERATIVE,
+    sample_level_fn=TTC_score_EM(),
+    corpus_level_fn=np.mean,
+)
+
+extend_enum(Metrics, "CUSTOM_TTC_F1", my_custom_ttc_f1_metric)
+extend_enum(Metrics, "CUSTOM_TTC_EM", my_custom_ttc_em_metric)
 
 if __name__ == "__main__":
     print("Imported metric")
---
+++ src/lighteval/tasks/multilingual/tasks/custom_tasks_ttc_tests.py
@@ -0,0 +1,212 @@
+from inspect_ai.dataset import Sample
+from inspect_ai.solver import generate, prompt_template
+
+from lighteval.metrics.dynamic_metrics import (
+    MultilingualQuasiExactMatchMetric,
+    MultilingualQuasiF1ScoreMetric,
+)
+from lighteval.metrics.metrics import Metrics, math_scorer
+from lighteval.tasks.lighteval_task import LightevalTaskConfig
+from lighteval.tasks.requests import Doc
+from lighteval.tasks.templates.qa import get_qa_prompt_function
+from lighteval.utils.language import Language
+
+
+def truthful_qa_generative_prompt(line, task_name: str = None):
+    correct_answers = [
+        answer.strip() + ("" if answer.endswith(".") else ".") for answer in line["correct_answers"] if answer != ""
+    ]
+
+    if "I have no comment." not in correct_answers:
+        correct_answers.append("I have no comment.")
+
+    incorrect_answers = [
+        answer.strip() + ("" if answer.endswith(".") else ".") for answer in line["incorrect_answers"] if answer != ""
+    ]
+
+    return Doc(
+        task_name=task_name,
+        query=f"Q: {line['question'].strip()}\nA:",
+        choices=correct_answers + incorrect_answers,
+        gold_index=list(range(len(correct_answers))),
+    )
+
+
+def drop_prompt(line, task_name: str = None):
+    def _flatten_validated_answers(validated_answers):
+        valid_answers = []
+        for i in range(len(validated_answers["number"])):
+            valid_answers.append(
+                {
+                    "number": validated_answers["number"][i],
+                    "date": validated_answers["date"][i],
+                    "spans": validated_answers["spans"][i],
+                }
+            )
+        return valid_answers
+
+    def parse_answer(answer):
+        if answer["number"] != "":
+            return (str(answer["number"]),)
+        if answer["spans"] != []:
+            return tuple(answer["spans"])
+        return (" ".join([answer["date"]["day"], answer["date"]["month"], answer["date"]["year"]]).strip(),)
+
+    answers = []
+    answers_set = set()
+    candidates = [line["answer"]] + _flatten_validated_answers(line["validated_answers"])
+    for candidate in candidates:
+        answer = parse_answer(candidate)
+        if answer in answers_set:
+            continue
+        answers.append(answer)
+        answers_set.add(answer)
+
+    is_few_shots = line.get("__few_shots", False)
+
+    return Doc(
+        task_name=task_name,
+        query=f"Passage: {line['passage']}\nQuestion: {line['question']}\nAnswer:",
+        choices=[f"{' ' if is_few_shots else ''}{', '.join(a)}" for a in answers],
+        gold_index=list(range(len(answers))),
+        specific={"golds_no_preprocessing": [list(a) for a in answers]},
+    )
+
+
+def coqa_prompt(line, task_name: str = None):
+    results = []
+    for q, a in zip(line["questions"], line["answers"]["input_text"]):
+        results.append(Doc(task_name=task_name, query=f"{line['story']} \n\nQ: {q}\n\nA: ", choices=[a], gold_index=0))
+    return results
+
+
+MATH_PROMPT_TEMPLATE = """
+Solve the following math problem step by step. The last line of your
+response should be of the form "ANSWER: $ANSWER" (without quotes)
+where $ANSWER is the answer to the problem.
+
+{prompt}
+
+Remember to put your answer on its own line at the end in the form
+"ANSWER: $ANSWER" (without quotes) where $ANSWER is the answer to
+the problem, and you do not need to use a \\boxed command.
+
+Reasoning:
+""".strip()
+
+
+def record_to_sample(record):
+    DELIM = "####"
+    input = record["question"]
+    answer = record["answer"].split(DELIM)
+    target = answer.pop().strip()
+    reasoning = DELIM.join(answer)
+    return Sample(input=input, target=target, metadata={"reasoning": reasoning.strip()})
+
+
+def sample_to_fewshot(sample):
+    return f"{sample.input}\n\nReasoning:\n" + f"{sample.metadata['reasoning']}\n\n" + f"ANSWER: {sample.target}"
+
+
+def gsm8k_prompt(line, task_name: str = None):
+    return Doc(
+        task_name=task_name,
+        query=f"Question: {line['question']}\nAnswer:",
+        choices=[f" {line['answer']}"],
+        gold_index=0,
+    )
+
+
+mytruthfulqa = LightevalTaskConfig(
+    name="mytruthfulqa",
+    prompt_function=truthful_qa_generative_prompt,
+    hf_repo="truthfulqa/truthful_qa",
+    hf_subset="generation",
+    hf_avail_splits=["validation"],
+    evaluation_splits=["validation"],
+    few_shots_split=None,
+    few_shots_select=None,
+    generation_size=200,
+    metrics=[Metrics.CUSTOM_TTC_F1, Metrics.CUSTOM_TTC_EM, Metrics.exact_match],
+    stop_sequence=["\n"],
+    version=0,
+)
+
+
+mydrop = LightevalTaskConfig(
+    name="mydrop",
+    prompt_function=drop_prompt,
+    hf_repo="lighteval/drop_harness",
+    hf_subset="default",
+    evaluation_splits=("validation",),
+    few_shots_split="train",
+    generation_size=250,
+    stop_sequence=["Question:", "question:", "\n"],
+    metrics=[Metrics.CUSTOM_TTC_F1, Metrics.CUSTOM_TTC_EM, Metrics.exact_match],
+    version=1,
+)
+
+mycoqa = LightevalTaskConfig(
+    name="mycoqa",
+    prompt_function=coqa_prompt,
+    hf_repo="stanfordnlp/coqa",
+    hf_subset="default",
+    hf_avail_splits=["train", "validation"],
+    evaluation_splits=["validation"],
+    stop_sequence=["\n", "Question:", "question:"],
+    generation_size=100,
+    version=1,
+    metrics=[Metrics.CUSTOM_TTC_F1, Metrics.CUSTOM_TTC_EM, Metrics.exact_match],
+)
+
+mygsm8k = LightevalTaskConfig(
+    name="mygsm8k",
+    prompt_function=gsm8k_prompt,
+    sample_fields=record_to_sample,
+    sample_to_fewshot=sample_to_fewshot,
+    solver=[prompt_template(MATH_PROMPT_TEMPLATE), generate(cache=True)],
+    scorer=math_scorer(),
+    hf_repo="openai/gsm8k",
+    hf_subset="main",
+    hf_avail_splits=["train", "test"],
+    evaluation_splits=["test"],
+    few_shots_split=None,
+    few_shots_select="random_sampling_from_train",
+    generation_size=256,
+    metrics=[
+        Metrics.CUSTOM_TTC_F1,
+        Metrics.CUSTOM_TTC_EM,
+        Metrics.expr_gold_metric,
+    ],
+    stop_sequence=["Question:"],
+    version=0,
+)
+
+
+mychegeka = LightevalTaskConfig(
+    name="mychegeka",
+    prompt_function=get_qa_prompt_function(
+        Language.RUSSIAN,
+        lambda line: {
+            "question": line["inputs"]["text"],
+            "choices": [line["outputs"]],
+        },
+    ),
+    hf_repo="ai-forever/MERA",
+    hf_subset="chegeka",
+    evaluation_splits=("test",),
+    hf_avail_splits=["train", "test"],
+    generation_size=400,
+    stop_sequence=[
+        "\n",
+    ],
+    metrics=[
+        Metrics.CUSTOM_TTC_F1,
+        Metrics.CUSTOM_TTC_EM,
+        MultilingualQuasiExactMatchMetric(Language.RUSSIAN, "prefix"),
+        MultilingualQuasiF1ScoreMetric(Language.RUSSIAN),
+    ],
+)
+
+
+TASKS_TABLE = [mytruthfulqa, mychegeka, mydrop, mycoqa, mygsm8k]
